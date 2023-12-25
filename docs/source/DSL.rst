6f34988324b7af39001d13410d10634fc04b465e
emofer <emofer233@gmail.com>
2023-12-25 23:04:39 +0800

    docs: sphinx doc framework

---
+++ docs/source/DSL.rst
@@ -0,0 +1,29 @@
+DSL package
+===========
+
+Submodules
+----------
+
+DSL.interpreter module
+----------------------
+
+.. automodule:: DSL.interpreter
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+DSL.parser module
+-----------------
+
+.. automodule:: DSL.parser
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: DSL
+   :members:
+   :undoc-members:
+   :show-inheritance:
---
+++ docs/source/conf.py
@@ -0,0 +1,36 @@
+import sys
+import os
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'YARD'
+copyright = '2023, emofer'
+author = 'emofer'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['recommonmark', 'sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'zh_CN'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+source_suffix = ['.rst', '.md', '.MD']
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+sys.path.insert(0, os.path.abspath('../../src'))
+# print(sys.path)
